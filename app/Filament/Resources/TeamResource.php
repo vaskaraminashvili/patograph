64185bcfefcf2b84769e26e182cbccb05097512c
vasili <vasilr@giftbasketsoverseas.com>
2024-05-31 16:50:07 +0400

    fin create modules

---
+++ app/Filament/Resources/TeamResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TeamResource\Pages;
+use App\Filament\Resources\TeamResource\RelationManagers;
+use App\Models\Team;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TeamResource extends Resource
+{
+    protected static ?string $model = Team::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema(Team::getForm());
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('surname')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('position')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->color(function ($state){
+                        return $state->getColor();
+                    })
+                    ->badge()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('sort')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTeams::route('/'),
+            'create' => Pages\CreateTeam::route('/create'),
+            'edit' => Pages\EditTeam::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -2,6 +2,10 @@
 
 namespace App\Models;
 
+use App\Enums\StatusEnums;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -29,6 +33,29 @@ class Plan extends Model
      */
     protected $casts = [
         'id' => 'integer',
+        'status' => StatusEnums::class,
         'deleted_at' => 'timestamp',
     ];
+
+
+    public static function getForm()
+    {
+        return [
+            TextInput::make('title')
+                ->required()
+                ->maxLength(255),
+            RichEditor::make('description')
+                ->required()
+                ->columnSpanFull(),
+            TextInput::make('status')
+                ->required()
+                ->maxLength(255),
+            FileUpload::make('img')
+                ->required()
+                ->columnSpanFull(),
+            TextInput::make('sort')
+                ->required()
+                ->numeric(),
+        ];
+    }
 }
